14663a72064dd1c3f53d4f9f9bd48e8e6b02fe92
gitzV <43180358+gitzV@users.noreply.github.com>
2024-11-01 03:37:45 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,8 +1,8 @@
 # variables.tf
 
-variable "snowflake_account" {}
-variable "snowflake_username" {}
-variable "snowflake_password" {}
-variable "snowflake_region" {}
-variable "database_name" {}
+#variable "snowflake_account" {}
+#variable "snowflake_username" {}
+#variable "snowflake_password" {}
+#variable "snowflake_region" {}
+#variable "database_name" {}
 
