8cb18b80706cc06c585915b2d7bcd1739c5217ca
gitzV <43180358+gitzV@users.noreply.github.com>
2024-10-31 22:40:40 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,14 @@
+# main.tf
+
+provider "snowflake" {
+  account  = var.snowflake_account
+  username = var.snowflake_username
+  password = var.snowflake_password
+  role     = "SYSADMIN"
+  region   = var.snowflake_region
+}
+
+resource "snowflake_database" "example_db" {
+  name = var.database_name
+}
 
