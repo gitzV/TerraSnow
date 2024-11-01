ec86c9f8e789bcb61df9da095b36a4e2e556cb7f
gitzV <43180358+gitzV@users.noreply.github.com>
2024-11-01 03:29:04 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,23 +1,47 @@
-# main.tf -
-
+# main.tf
 terraform {
   required_providers {
     snowflake = {
       source  = "Snowflake-Labs/snowflake"
-      version = "0.43.0" # adjust this to the latest version as needed
+      version = "~> 0.87.1"  # Use the latest stable version
     }
   }
 }
 
 provider "snowflake" {
-  account  = var.snowflake_account
-  username = var.snowflake_username
-  password = var.snowflake_password
-  role     = "SYSADMIN"
-  region   = var.snowflake_region
+  account    = var.snowflake_account
+  username   = var.snowflake_username
+  password   = var.snowflake_password
+  region     = var.snowflake_region
+  role       = "ACCOUNTADMIN"  # Or another appropriate role
+  
+  # Specify authentication method
+  authenticator = "snowflake"  # For username/password auth
 }
 
-resource "snowflake_database" "example_db" {
-  name = var.database_name
+# Define your variables
+variable "snowflake_account" {
+  type        = string
+  description = "Snowflake account identifier"
 }
 
+variable "snowflake_username" {
+  type        = string
+  description = "Snowflake username"
+}
+
+variable "snowflake_password" {
+  type        = string
+  sensitive   = true
+  description = "Snowflake password"
+}
+
+variable "snowflake_region" {
+  type        = string
+  description = "Snowflake region"
+}
+
+variable "database_name" {
+  type        = string
+  description = "Name of the database to create"
+}
