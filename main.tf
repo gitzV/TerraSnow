462487d936dd3592c2dc7157f77d539634ef3089
gitzV <43180358+gitzV@users.noreply.github.com>
2024-11-09 04:42:54 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -139,7 +139,7 @@ resource "snowflake_file_format" "csv_format" {
 }
 
 ##
-
+/*
 # Install SnowSQL
 resource "null_resource" "install_snowsql" {
   provisioner "local-exec" {
@@ -174,7 +174,7 @@ resource "null_resource" "install_snowsql" {
     EOF
   }
 }
-
+*/
 
 # Check SnowSQL version with the corrected path
 resource "null_resource" "check_snowsql_version" {
@@ -183,7 +183,7 @@ resource "null_resource" "check_snowsql_version" {
   }
 
   depends_on = [
-    null_resource.install_snowsql,
+   # null_resource.install_snowsql,
     snowflake_stage.internal_stage
   ]
 }
