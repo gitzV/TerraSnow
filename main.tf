18eae0d47260fd8361befa0b287fb86f704a4c82
gitzV <43180358+gitzV@users.noreply.github.com>
2024-11-25 10:05:11 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -25,6 +25,22 @@ resource "snowflake_role" "dev_role" {
 }
 
 
+# Create a warehouse
+resource "snowflake_warehouse" "warehouse" {
+  name           = var.snowflake_WH
+  warehouse_size = var.snowflake_WH_SIZE
+  auto_suspend   = 60
+  depends_on = [snowflake_role.dev_role]
+}
+
+# Grant warehouse usage to role
+resource "snowflake_warehouse_grant" "warehouse_grant" {
+  warehouse_name = snowflake_warehouse.warehouse.name
+  privilege      = "USAGE"
+  roles         = [snowflake_role.dev_role.name,"ACCOUNTADMIN"]
+  depends_on     = [snowflake_warehouse.warehouse, snowflake_role.dev_role]
+}
+
 
 # Outputs
 
